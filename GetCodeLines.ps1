6f8ddff06020c5161bbb4342d91a253292590835
Ryan Languay <Ryan Languay>
2016-11-14 17:31:19 -0500

    Initial commit

---
+++ GetCodeLines.ps1
@@ -0,0 +1,6 @@
+﻿$srcExt = @('.js','.cpp','.c','.h','.py')
+$srcFiles = Get-ChildItem "C:\firefox-49.0.2" -Recurse -File | Where-Object {$_.Extension -in $srcExt}
+$totalLines = 0
+$srcFiles | ForEach-Object {
+    $totalLines += $($_ | Get-Content | Measure-Object -Line).Count
+}
