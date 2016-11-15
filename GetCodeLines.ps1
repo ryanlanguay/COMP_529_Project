96c5007ef67dcb96499820203f7853657170b109
Ryan Languay <Ryan Languay>
2016-11-14 19:30:44 -0500

    Updated script to search patterns in lines

--- GetCodeLines.ps1
+++ GetCodeLines.ps1
@@ -1,6 +1,46 @@
-﻿$srcExt = @('.js','.cpp','.c','.h','.py')
+﻿# Source code file extensions ot search for (Firefox is implemented in C++, C, and JavaScript)
+$srcExt = @('.js','.cpp','.c','.h')
+# Regex patterns for comments, functions, and objects in C++
+$commentPatterns = '/\**|//*|\*\**|\*/'
+$functionPattern = '.*\){*|.*\(`r`n{*'
+$objPattern = '.* *;'
+# Get all source files in the src directory
 $srcFiles = Get-ChildItem "C:\firefox-49.0.2" -Recurse -File | Where-Object {$_.Extension -in $srcExt}
 $totalLines = 0
-$srcFiles | ForEach-Object {
-    $totalLines += $($_ | Get-Content | Measure-Object -Line).Count
+$commentLines = 0
+$functions = 0
+$objects = 0
+ForEach ($file in $srcFiles)
+{
+    # Check each line in this file for comments, functions, and objects. Read 1000 at a time for performance reasons
+    Get-Content $file.FullName -ReadCount 1000 | ForEach-Object {
+        $totalLines += $_.Length
+        switch ($_)
+        {
+            ({$_ -match $commentPatterns})
+            {
+                $commentLines++
+                break
+            }
+            ({$_ -match $functionPattern})
+            {
+                $functions++
+                break
+            }
+            ({$_ -match $objPattern})
+            {
+                $objects++
+                break
+            }
+            default
+            {
+                break
+            }
+        }
+    }
 }
+Write-Host "Total lines of code: $totalLines
+Commented lines: $commentLines
+Functions: $functions
+Objects: $objects
+"
